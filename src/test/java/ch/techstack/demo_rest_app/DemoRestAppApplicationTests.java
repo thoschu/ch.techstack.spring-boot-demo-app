12c897da18d660ded3594f3b7ad49cd745681017
Thomas-Laurenz Schulte <thomas-laurenz.schulte@adesso.de>
2024-07-05 13:39:42 +0200

    add some code snippets

---
+++ src/test/java/ch/techstack/demo_rest_app/DemoRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.techstack.demo_rest_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
