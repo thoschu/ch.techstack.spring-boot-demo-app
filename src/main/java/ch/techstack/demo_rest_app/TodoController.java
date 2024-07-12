31956cc33f34ed02a813c2792cd3fd3a589f2241
Thomas-Laurenz Schulte <thomas-laurenz.schulte@adesso.de>
2024-07-12 18:11:21 +0200

    refactor

--- src/main/java/ch/techstack/demo_rest_app/TodoController.java
+++ src/main/java/ch/techstack/demo_rest_app/TodoController.java
@@ -1,7 +1,9 @@
 package ch.techstack.demo_rest_app;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
 
 @RestController
 public class TodoController {
