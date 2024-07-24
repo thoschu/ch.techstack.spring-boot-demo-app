033711e608ee4deba7778af28639fc77e3249fa7
Thomas-Laurenz Schulte <thomas-laurenz.schulte@adesso.de>
2024-07-24 13:51:55 +0200

    refactor

--- src/test/java/ch/techstack/demo_rest_app/DemoRestAppApplicationTests.java
+++ src/test/java/ch/techstack/demo_rest_app/DemoRestAppApplicationTests.java
@@ -1,13 +1,19 @@
 package ch.techstack.demo_rest_app;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class DemoRestAppApplicationTests {
 
+	@Autowired
+	private TodoController todoController;
+
 	@Test
 	void contextLoads() {
+		assertThat(todoController).isNotNull();
 	}
-
 }
